6b7d9e2b777029c3dfa53ba4f1ca196e10a14c39
Olle2700 <165487458+Olle2700@users.noreply.github.com>
2025-12-09 15:18:08 +0100

    Opdateringer

--- app.py
+++ app.py
@@ -875,4 +875,4 @@ def sync_languages():
 # MAIN
 ##############################
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
